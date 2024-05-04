7c01f237bdfe9331569996507f7596a751dad23b
akayer19 <akayeauto@gmail..com>
2024-05-04 15:08:31 -0700

    Added service worker and run program

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -2,60 +2,64 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
-      main: './client/src/index.js',  // Assuming your main entry is in 'client/src'
-      install: './client/src/install.js'  // Assuming you have an install script
+      main: './src/js/index.js',
+      install: './src/js/install.js'
     },
     output: {
       filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'client/dist'),  // Output in 'client/dist'
+      path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './client/index.html'  // Assuming your HTML template is in 'client'
+        template: './index.html',
+        title: 'J.A.T.E'
       }),
       new InjectManifest({
-        swSrc: './client/src-sw.js',  // Service worker source in 'client'
-        swDest: 'service-worker.js'
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
       }),
       new WebpackPwaManifest({
-        name: 'Text Editor PWA',
-        short_name: 'TextEditor',
-        description: 'A PWA Text Editor that works offline and saves data locally.',
-        background_color: '#ffffff',
-        crossorigin: 'use-credentials', // can be null, use-credentials or anonymous
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
-            src: path.resolve('client/icons/icon.png'), // Assuming your icons are in 'client/icons'
-            sizes: [96, 128, 192, 256, 384, 512] // Multiple sizes
-          }
-        ]
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       }),
-      new MiniCssExtractPlugin({
-        filename: 'styles.css'  // Output CSS filename
-      })
     ],
+
     module: {
       rules: [
         {
-          test: /\.css$/,
-          use: [MiniCssExtractPlugin.loader, 'css-loader']  // CSS loader to handle .css files
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
         },
         {
-          test: /\.js$/,
+          test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env']  // Preset used for modern JavaScript features
-            }
-          }
-        }
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
